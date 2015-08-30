10fdf0285527f4faf256493674f225bc42f08ffc
Quinn Evans <heddwch@lambdaos.org>
2015-08-30 01:02:40 -0600

    Add keybind for magit-status

--- init/99-global-keybinds.el
+++ init/99-global-keybinds.el
@@ -1,3 +1,4 @@
 (define-key ctl-x-4-map (kbd "o") 'swap-window-positions)
 (define-key ctl-x-4-map (kbd "q") 'quit-other-window)
 (global-set-key (kbd "H-k") 'kill-ring-save-line)
+(global-set-key (kbd "C-x g") 'magit-status)
