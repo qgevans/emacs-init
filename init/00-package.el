46097bdd55f8fd5dd3c7da0273918d6932eca146
Quinn Evans <heddwch@lambdaos.org>
2015-12-20 08:34:27 -0700

    Updates, add MELPA, install rust-mode

--- init/00-package.el
+++ init/00-package.el
@@ -1,5 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
+	     '("melpa" .
+	       "http://melpa.org/packages/"))
+(add-to-list 'package-archives
 	     '("marmalade" .
 	       "http://marmalade-repo.org/packages/"))
 (package-initialize)
